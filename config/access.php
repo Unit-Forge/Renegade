9f346ece20fcd79a037778b409b17c6e4cae4f69
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2017-02-17 11:23:15 -0700

    Namespace modification

--- config/access.php
+++ config/access.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Models\Access\Role\Role;
-use App\Models\Access\Permission\Permission;
+use Renegade\Models\Access\Role\Role;
+use Renegade\Models\Access\Permission\Permission;
 
 return [
     /*
