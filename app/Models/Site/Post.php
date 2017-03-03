2dd5190f6fbf520ce73565af11b3ec6c48533fd1
Guillermo Rodriguez <rodriguez.guillermo.a42@gmail.com>
2017-03-02 19:44:03 -0700

    Added Post Model fields

--- app/Models/Site/Post.php
+++ app/Models/Site/Post.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['author_id', 'category_id','title','seo_title','excerpt','body','meta_description','meta_keywords','status'];
+
+    /**
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function category()
+    {
+        return $this->belongsTo('Renegade\Models\Site\Category');
+    }
 }
